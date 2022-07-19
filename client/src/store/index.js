d736b486d4717547dec86fd5755f98dece3cb810
mariapaulav <mariapaulaviviz@gmail.com>
2022-07-19 09:29:38 -0500

    deploy app

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
-import { applyMiddleware,compose, legacy_createStore as createStore} from 'redux'
+import { applyMiddleware,/* compose ,*/ legacy_createStore as createStore} from 'redux'
 
 import reducer from './reducer'
 import thunk from 'redux-thunk' 
 
 const store = createStore(
     reducer, 
-    //applyMiddleware(thunk)
-    compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    applyMiddleware(thunk)
+    // compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
     )
 export default store
 
